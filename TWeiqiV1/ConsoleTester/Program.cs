b18c365f8b1ca7ffa7072ff2c9442381f93def2d
sunchun1979 <sunchun79@gmail.com>
2014-06-15 23:23:40 -0400

    timing test

--- TWeiqiV1/ConsoleTester/Program.cs
+++ TWeiqiV1/ConsoleTester/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,7 @@ namespace ConsoleTester
         static void Main(string[] args)
         {
             const int size = 19;
-            BitBoard b = new BitBoard(size);
+            const int totalMove = 200;
 
             ////b.Move19(3, 3, BitBoard.BLACK);
             ////b.Move19(1, 1, BitBoard.WHITE);
@@ -23,9 +24,23 @@ namespace ConsoleTester
             ////Console.WriteLine(b);
             ////b.Move19(2, 3, BitBoard.WHITE);
 
-            var rand = new Random(1001);
+            var rand = new Random();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                RandomGame(size, totalMove, rand);
+            }
+            sw.Stop();
+            Console.WriteLine("Elapsed = {0}", sw.Elapsed);
+        }
+
+        private static void RandomGame(int size, int totalMove, Random rand)
+        {
+            BitBoard b = new BitBoard();
             int move = BitBoard.BLACK;
-            for (int i = 0; i < 260; i++)
+            for (int i = 0; i < totalMove; i++)
             {
                 bool legal = false;
                 while (!legal)
